3f69001111b8cadda9e0d09b2dcf5992b7fc9e6d
ValentinSiderskiyPhD <vs611@gsbs.rutgers.edu>
2020-01-31 09:06:17 -0500

    Added Camera setup

--- LASKIK_FIX/Assets/FixView.cs
+++ LASKIK_FIX/Assets/FixView.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class FixView : MonoBehaviour
 {
+    public RenderTexture rt;
+    int mWidth = 1280;
+    int mHeight = 720;
+
+    // The "m_Display" is the GameObject whose Texture will be set to the captured image.
+    public Renderer m_Display;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
+
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
+
+
+
+
 }
